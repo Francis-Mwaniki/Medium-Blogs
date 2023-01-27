a146dffa320c4e869cf940d59efb2d2f37713408
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-27 12:43:21 +0300

    new

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+let q = ref('')
+const { result, search } = useAlgoliaSearch('test_index')
+onMounted(async () => {
+  await search({ query: q.value })
+})
+</script>
+<template>
+    <input type="search" name="" class=" ring" v-model="q" id="">
+  <div class=" bg-slate-800 ring-red-300 ring text-black">{{ result }}</div>
+</template>
