ed493c041a213199c715fa96e2b3be963a48e6a2
Francis-Mwaniki <francismwaniki630>
2023-03-23 23:55:53 +0300

    -all data cleaned

--- components/blog.vue
+++ components/blog.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="mb-8">
-    <img :src="post.image" class="mb-4 rounded-lg" />
+    <!-- author -->
+    <div class="flex items-center mb-2">
+      <img :src="post.image" class="w-10 h-10 rounded-full mr-2" alt="avatar" />
+      <span class="text-gray-600 text-sm">{{ post.author }}</span>
+    </div>
     <div class="flex items-center mb-2">
       <span class="text-gray-600 text-sm">{{ post.date }}</span>
       <span class="mx-2 text-gray-400">•</span>
       <span class="text-gray-600 text-sm">{{ post.readTime }} min read</span>
     </div>
-    <h2 class="text-2xl font-semibold mb-2">
-      <nuxt-link :to="`/blog/${post.slug}`">{{ post.title }}</nuxt-link>
+    <h2 class="text-2xl font-semibold mb-2 text-indigo-700">
+      <a :href="`${post.slug}`">{{ post.title }}...</a>
     </h2>
     <p class="text-gray-600">{{ post.excerpt }}</p>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,62 @@
 import Blog from "~~/components/blog.vue";
 
 const posts = ref([]);
+const loading = ref(true);
+type typeOfPosts = {
+  title: string;
+  author: string;
+  slug: string;
+  excerpt: string;
+  date: string;
+  image: string;
+  readTime: string;
+};
+const cleanedPosts = ref<typeOfPosts[]>([]);
 const fetchBlogPost = async () => {
+  loading.value = true;
   let post_url = "http://localhost:4000/tasks";
   try {
     let response = await fetch(post_url);
     if (response.ok) {
       let data = await response.json();
-      posts.value = <any>{
-        title: data.items[""],
-        slug: data.slug,
-        excerpt: data.excerpt,
-        date: data.date,
-        readTime: data.readTime,
-        image: data.image,
-      };
-      console.log(data);
+      posts.value = data.result.robotTasks.items[0].capturedLists["Posts list"];
+      cleanedPosts.value = posts.value.map((post) => {
+        return {
+          title: post["Title"],
+          author: post["Author"],
+          slug: post["Post link"],
+          excerpt: post["Description"],
+          date: post["Post Date"],
+          image: post["Image URL"] || "https://via.placeholder.com/150",
+          readTime: post["Time to read"],
+        };
+      });
+      loading.value = false;
+      console.log(cleanedPosts.value);
     } else {
+      loading.value = false;
       let data = await response.json();
-      console.log(`Error occurred`);
+      console.log(`Error occurred while fetching data: ${data}`);
     }
   } catch (error) {}
 };
-fetchBlogPost();
-onMounted(async () => {});
+
+onMounted(async () => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 3000);
+  await fetchBlogPost();
+});
 </script>
 <template>
   <div>
     <!-- results  with tailwindcss-->
 
     <div class="max-w-3xl mx-auto mt-8">
-      <div v-for="post in posts" :key="post">
+      <Loader :loading="loading" />
+      <h1 class="text-4xl font-semibold mb-8 text-center">Blog</h1>
+
+      <div v-for="post in cleanedPosts">
         <Blog :post="post" />
       </div>
     </div>
